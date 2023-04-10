33b4f037866f2a59a6f2a1e8089858696802ee15
aayushxrj <aayush.ranjan0904@gmail.com>
2023-04-11 01:33:11 +0530

    integrated openai in bot

---
+++ bot2.py
@@ -0,0 +1,40 @@
+
+import discord
+from discord.ext import commands
+import openai
+
+import json
+with open('token.json','r') as f:
+    key = json.load(f)
+
+intents = discord.Intents.all()
+intents.members = True
+intents.messages = True
+intents.guilds = True
+intents.reactions = True
+intents.presences = True
+
+client = commands.Bot(command_prefix='!', intents=intents)
+
+openai.api_key = key["openai"]
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return 
+    
+    prompt = "The user said: " + message.content
+    print(prompt)
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=50
+    )
+
+    await message.channel.send(response.choices[0].text)
+
+client.run(key["discord"])
